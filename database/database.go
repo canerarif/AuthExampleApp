d3ccbfba32f1fe6d9e57dc60fcecbe67d54966b5
Caner Arifoğlu <caner.arifoglu@gmail.com>
2022-01-15 22:50:06 +0300

    first commit

---
+++ database/database.go
@@ -0,0 +1,21 @@
+package database
+
+import (
+	"../models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Connect() {
+	connection, err := gorm.Open(postgres.Open("host=localhost user=gorm password=gorm dbname=gorm port=9920 sslmode=disable TimeZone=Europe/Istanbul"), &gorm.Config{})
+
+	if err != nil {
+		panic("could not connect to the database")
+	}
+
+	DB = connection
+
+	connection.AutoMigrate(&models.User{})
+}
